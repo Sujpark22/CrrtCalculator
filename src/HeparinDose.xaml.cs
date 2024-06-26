f2859d283bb7eb6e483c90d1b45b39ebf2bb3021
Hadi Fooladi Talari <hadi.f7@gmail.com>
2024-06-26 15:04:54 -0400

    Applying MVVM

--- src/HeparinDose.xaml.cs
+++ src/HeparinDose.xaml.cs
@@ -7,88 +7,19 @@ namespace CRRT_Calculator
 {
     public partial class HeparinDose : ContentPage
     {
-        private readonly double hep10;
-        private readonly double hep20;
-        private readonly double hep30;
         public HeparinDose(string heparin, string weight)
         {
             InitializeComponent();
 
-            if (heparin != null)
-            {
-                double weightD = double.Parse(weight);
-                //heparin 10
-                hep10 = (heparin == "Yes") ? 10 * weightD : 0;
-                hep10Label.Text = $"Heparin 10 units/kg: {hep10}";
-
-                //heparin 20
-                hep20 = (heparin == "Yes") ? 20 * weightD : 0;
-                hep20Label.Text = $"Heparin 20 units/kg: {hep20}";
-
-                //heparin30
-                hep30 = (heparin == "Yes") ? 30 * weightD : 0;
-                hep30Label.Text = $"Heparin 30 units/kg: {hep30}";
-            }
-
+			_model = new ViewModels.HeparinDose(heparin, weight);
+            BindingContext = _model;
         }
+
+        readonly ViewModels.HeparinDose _model;
 
         private void OnCalculateClicked(object sender, EventArgs e)
         {
-
-            string preACT = preACTEntry.Text;
-            string prePTT = prePTTEntry.Text;
-
-            
-            //Suggested heparin bolus (based on ACT)
-            string bolusACT;
-            double preACTD = double.Parse(preACT);
-            if (preACTD <= 120)
-            {
-                bolusACT = hep30.ToString();
-            }
-            else if (preACTD > 120 && preACTD < 140)
-            {
-                bolusACT = hep20.ToString();
-            }
-            else if (preACTD >= 140 && preACTD < 160)
-            {
-                bolusACT = hep10.ToString();
-            }
-            else if (preACTD >= 160)
-            {
-                bolusACT = "HOLD BOLUS i.e. 0";
-            }
-            else
-            {
-                bolusACT = "Unknown";
-            }
-            bolusACTLabel.Text = $"Suggested heparin bolus (based on ACT):     {bolusACT}";
-
-
-            //Suggested heparin bolus (based on PTT)
-            string bolusPTT;
-            double prePTTD = double.Parse(prePTT);
-            if (prePTTD <= 15)
-            {
-                bolusPTT = hep30.ToString();
-            }
-            else if (prePTTD > 15 && prePTTD <= 30)
-            {
-                bolusPTT = hep20.ToString();
-            }
-            else if (prePTTD > 30 && prePTTD < 40)
-            {
-                bolusPTT = hep10.ToString();
-            }
-            else if (prePTTD >= 40)
-            {
-                bolusPTT = "HOLD BOLUS i.e. 0";
-            }
-            else
-            {
-                bolusPTT = "Unknown";
-            }
-            bolusPTTLabel.Text = $"Suggested heparin bolus (based on PTT):     {bolusPTT}";
+            _model.Calculate();
         }
     }
 }
---
+++ src/ViewModels/HeparinDose.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CRRT_Calculator.ViewModels;
+
+class HeparinDose : INotifyPropertyChanged
+{
+	public HeparinDose(string? heparin, string weight)
+	{
+		if (heparin != null)
+		{
+			double weightD = double.Parse(weight);
+			//heparin 10
+			hep10 = (heparin == "Yes") ? 10 * weightD : 0;
+			hep10Label = $"Heparin 10 units/kg: {hep10}";
+
+			//heparin 20
+			hep20 = (heparin == "Yes") ? 20 * weightD : 0;
+			hep20Label = $"Heparin 20 units/kg: {hep20}";
+
+			//heparin30
+			hep30 = (heparin == "Yes") ? 30 * weightD : 0;
+			hep30Label = $"Heparin 30 units/kg: {hep30}";
+		}
+	}
+
+	readonly double hep10, hep20, hep30;
+
+	string _bolusACT, _bolusPTT;
+
+	public event PropertyChangedEventHandler? PropertyChanged;
+
+	public void OnPropertyChanged([CallerMemberName] string name = "") =>
+		PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+
+	public string hep10Label { get; }
+	public string hep20Label { get; }
+	public string hep30Label { get; }
+
+	public string preACT { get; set; }
+	public string prePTT { get; set; }
+
+	public string bolusACT
+	{
+		get => _bolusACT;
+		private set
+		{
+			_bolusACT = value;
+			OnPropertyChanged();
+		}
+	}
+
+	public string bolusPTT
+	{
+		get => _bolusPTT;
+		private set
+		{
+			_bolusPTT = value;
+			OnPropertyChanged();
+		}
+	}
+
+	public void Calculate()
+	{
+		//Suggested heparin bolus (based on ACT)
+		string bolusACT;
+		double preACTD = double.Parse(preACT);
+		if (preACTD <= 120)
+		{
+			bolusACT = hep30.ToString();
+		}
+		else if (preACTD > 120 && preACTD < 140)
+		{
+			bolusACT = hep20.ToString();
+		}
+		else if (preACTD >= 140 && preACTD < 160)
+		{
+			bolusACT = hep10.ToString();
+		}
+		else if (preACTD >= 160)
+		{
+			bolusACT = "HOLD BOLUS i.e. 0";
+		}
+		else
+		{
+			bolusACT = "Unknown";
+		}
+		this.bolusACT = $"Suggested heparin bolus (based on ACT):     {bolusACT}";
+
+
+		//Suggested heparin bolus (based on PTT)
+		string bolusPTT;
+		double prePTTD = double.Parse(prePTT);
+		if (prePTTD <= 15)
+		{
+			bolusPTT = hep30.ToString();
+		}
+		else if (prePTTD > 15 && prePTTD <= 30)
+		{
+			bolusPTT = hep20.ToString();
+		}
+		else if (prePTTD > 30 && prePTTD < 40)
+		{
+			bolusPTT = hep10.ToString();
+		}
+		else if (prePTTD >= 40)
+		{
+			bolusPTT = "HOLD BOLUS i.e. 0";
+		}
+		else
+		{
+			bolusPTT = "Unknown";
+		}
+		this.bolusPTT = $"Suggested heparin bolus (based on PTT):     {bolusPTT}";
+	}
+}
